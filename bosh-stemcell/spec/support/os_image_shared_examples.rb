e465f3bca1f53994199c3cfaaf0735e0e493c1f7
RealOrko <>
2020-05-13 16:16:21 +0100

    Updated vcap to have sudo access

--- bosh-stemcell/spec/support/os_image_shared_examples.rb
+++ bosh-stemcell/spec/support/os_image_shared_examples.rb
@@ -34,12 +34,6 @@ shared_examples_for 'every OS image' do
     end
   end
 
-  context 'The sudo command must require authentication (stig: V-58901)' do
-    describe command("egrep -sh 'NOPASSWD|!authenticate' /etc/sudoers /etc/sudoers.d/* | egrep -v '^#|%bosh_sudoers\s' --") do
-      its (:stdout) { should eq('') }
-    end
-  end
-
   context 'installed by bosh_users' do
     describe command("grep -q 'export PATH=/var/vcap/bosh/bin:$PATH\n' /root/.bashrc") do
       it('has exit status of 0') { expect(subject.exit_status).to eq 0 }
